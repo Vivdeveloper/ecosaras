15bac5f332fd360d7e155fd8cef0359390444d0d
pragatid20 <pragati.d@logicaldna.com>
2024-12-06 17:38:57 +0530

    |ecosaras|add two new column in report of kra|

--- ecosaras/ecosaras/report/kra/kra.py
+++ ecosaras/ecosaras/report/kra/kra.py
@@ -7,7 +7,9 @@ import frappe
 def execute(filters=None):
 	columns = [
           {"fieldname": "parent", "label": "Title", "fieldtype": "Link", "options": "KRA", "width": 100},
-    {"fieldname": "week", "label": "Week", "fieldtype": "Date", "width": 150},
+    {"fieldname": "week", "label": "Week", "fieldtype": "Data", "width": 150},
+    {"fieldname": "from_date", "label": "From Date", "fieldtype": "Date", "width": 150},
+    {"fieldname": "to_date", "label": "To Date", "fieldtype": "Date", "width": 150},
     {"fieldname": "particular", "label": "Particular", "fieldtype": "Data","width": 100},
     {"fieldname": "priority", "label": "Priority", "fieldtype": "Data", "width": 150},
     {"fieldname": "planned_target", "label": "Priority Target", "fieldtype": "Int", "width": 150},
@@ -28,20 +30,13 @@ def report_data(filters):
     if kra:
         cond.append(f"parent = '{kra}'")
     
-    # Building condition for from_date
-    if from_date:
-        cond.append(f"week >= '{from_date}'")
-    
-    # Building condition for to_date
-    if to_date:
-        cond.append(f"week <= '{to_date}'")
-
+   
     # Combine all conditions with 'AND' and ensure there's always a valid condition
     cond = " AND ".join(cond) if cond else "1=1"
 
     # Query with dynamic conditions
     report_qry_data = frappe.db.sql(f"""
-        SELECT parent, week, particular, priority, planned_target, 
+        SELECT parent, week,from_date, to_date, particular, priority, planned_target, 
                achieved_target, progress_in_percentage, recovery_action_plan
         FROM `tabKPI`
         WHERE {cond}
