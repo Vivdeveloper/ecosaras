e576f07edfc5c99a712d2368fa62834b0f79b3b7
Pragati <pragati@mail.hybrowlabs.com>
2024-12-29 19:04:31 +0530

    |ecosaras|

--- ecosaras/override/purchase_receipt_dashboard.py
+++ ecosaras/override/purchase_receipt_dashboard.py
@@ -5,12 +5,4 @@ from frappe.utils import getdate, nowdate, nowtime, flt
 
 @frappe.whitelist()
 def get_quality_inspection_counts(purchase_receipt_no, filters=None):
-    if not filters:
-        filters = {}
-
-    filters.update({
-        "reference_name": ["=", purchase_receipt_no],
-        "docstatus": ["in", [0, 1, 2]]  # Draft, Submitted, Cancelled
-    })
-
-    return frappe.get_all('Quality Inspection', filters=filters, fields=['name'], as_list=True)
+   pass
